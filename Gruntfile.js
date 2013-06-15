9cfe34a0998b785d0b8c93d3722a7be65b7d5db9
Trevor Lohrbeer <trevor@fastfedora.com>
2013-06-15 00:01:21 -0400

    Updated build process to include concat plugin & provide additional meta data.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,68 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    
+    meta : {
+      banner :  "/**\n" +
+              " * <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today(\"m/d/yyyy\") %>\n" +
+              " * <%= pkg.homepage %>\n" +
+              " * Copyright (c) <%= grunt.template.today(\"yyyy\") %> <%= pkg.authors %>;\n" +
+              " * License: <%= pkg.license %>\n" +
+              " * <%= pkg.licenseUrl %>\n" +
+              " */\n",
+    
+      lastbuild : "<%= grunt.template.today(\"yyyy/mm/dd hh:ss\") %>"
+    },
+    
+    
+    concat : {
+      options: {
+        banner : "<%= meta.banner %>"
+      },
+
+      fullnodeps: {
+        dest: "dist/open-dashboard.<%= pkg.version %>.js",
+        src: [
+          "<%= concat.options.banner %>",
+          "src/simple-dashboard.js"
+        ]
+      },
+
+      fulldeps: {
+        dest: "dist/open-dashboard.deps.<%= pkg.version %>.js",
+        src : [
+          "<%= concat.options.banner %>",
+          "lib/miso.ds.deps.0.4.0.js",
+          "lib/d3.v3.js",
+          "dist/open-dashboard.<%= pkg.version %>.js"
+        ]
+      },
+
+      buildstatus : {
+        options : {
+          banner : "<%= grunt.template.today(\"yyyy/mm/dd hh:ss\") %>"
+        },
+        dest : "dist/LASTBUILD",
+        src : [
+          "<%= 'lastbuild' %>"
+        ]
+      }
+    },
+    
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: "<%= meta.banner %>"
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src : "src/<%= pkg.name %>.js",
+        dest: "build/<%= pkg.name %>.min.js"
       }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-contrib-concat");
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['concat', 'uglify']);
 
 };
